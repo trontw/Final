af9bd963d699ee042d8924583cfed74c4aff1193
root <root@student2.cs.appstate.edu>
2022-12-04 18:44:18 -0500

    Part 1 finished - testing left to do.

--- BinaryNode.java
+++ BinaryNode.java
@@ -1,24 +1,37 @@
 // Basic BinaryNode class, Feel free to edit this case.
 
 public class BinaryNode {
+    private String[] name = null;
     private String label; // The label of the node
     private BinaryNode[] childs; // childs[0] points to the left child, and childs[1] points to the right child
     private int[] weights; // weights[0] is the funness of the left ski path from here, and weights[1]
 	// is the funness of the right ski path from here.
 	private boolean edge;
 
-    // Feel free to add extra data fields and methods to cater to your implementation.
-    // However, please do not edit anything that is already provided, as these are needed
-	// to build the tree from the input.
 
+    // Feel free to add extra data fields and methods to 
+    // cater to your implementation.
+    // However, please do not edit anything that is already provided, 
+    // as these are needed to build the tree from the input.
+ 
 	// Constructor
     public BinaryNode(String label, BinaryNode left, BinaryNode right, int lw, int rw) {
         this.label = label;
         this.childs = new BinaryNode[]{left, right};
         this.weights = new int[]{lw, rw};
+        int size = 20;
+        this.name = new String[size];
     }
 
-	// Accessors and Mutators
+    public String[] getBackPtr(int i) { 
+        return name;
+    }
+
+    public void setbackPtr(String[] name2){ 
+        this.name = name2;
+    }
+	// Accessors and Mutators 
+
     public BinaryNode getLeftChild() {
         return childs[0];
     }
@@ -39,8 +52,8 @@ public class BinaryNode {
         return weights[0];
     }
 
-    public void setLeftWeight(int child) {
-        this.weights[0] = child;
+    public void setLeftWeight(int child) { 
+        this.weights[0] = child; 
     }
 
     public int getRightWeight() {
--- FunSki.java
+++ FunSki.java
@@ -3,6 +3,7 @@ public class FunSki {
     private static int fun = 0;
     private static int Rw = 0;
     private static int Lw = 0;
+    private static int size = 0;
     private static BinaryNode newRoot;
     private static BinaryNode Lchild;
     private static BinaryNode Rchild;
@@ -11,60 +12,66 @@ public class FunSki {
         int x = Lw - Rw;
         return x;
     }
-
+    /*
     public static void print(BinaryNode root2){
         //print(root2);
-        System.out.println(root2);
+        for (int i = 0; i < size; i++)
+            System.out.println(root);
     }
-
+    */
     public static void main(String[] args){
         BinaryTree tree = new BinaryTree();
         BinaryNode root = tree.getRoot();
         
         // Code Starts here...
-        System.out.println("Root is = "+root);
-        System.out.println("Root Label is = "+root.getLabel());
-        System.out.println("Left Child is = "+root.getLeftChild());
-        System.out.println("Right Child is = "+root.getRightChild());
-        System.out.println("Left Child weight is = "+root.getLeftWeight());
-        System.out.println("Right Child weight is = "+root.getRightWeight());
+        //System.out.println("Root is = "+root);
+        //System.out.println("Root Label is = "+root.getLabel());
+        //System.out.println("Left Child is = "+root.getLeftChild());
+        //System.out.println("Right Child is = "+root.getRightChild());
+        //System.out.println("Left Child weight is = "+root.getLeftWeight());
+        //System.out.println("Right Child weight is = "+root.getRightWeight());
 
         int Lw = root.getLeftWeight();
         int Rw = root.getRightWeight();
-
-
+        String[] arr = new String[20]; 
+        BinaryNode old_root = root;
         while (root.getLeftWeight() > 0 || root.getRightWeight() > 0){
             if (compareTo(Lw, Rw) > 0){               
-                System.out.println("<--- Go Left Marty");
-                System.out.println("Lw is = "+Lw);
+                //System.out.println("<--- Go Left Marty");
+                //System.out.println("Lw is = "+Lw);
                 fun += Lw;
                 newRoot = root.getLeftChild();
-                System.out.println("New root is: "+newRoot.getLabel());
+                //System.out.println("New root is: "+newRoot.getLabel());
                 root = newRoot;
                 Lchild = root.getLeftChild();
-                System.out.println("Left child of newRoot is: "+Lchild);
+                //System.out.println("Left child of newRoot is: "+Lchild);
                 Rchild = root.getRightChild();
-                System.out.println("Right child of newRoot is: "+Rchild);
+                //System.out.println("Right child of newRoot is: "+Rchild);
                 Lw = root.getLeftWeight();
                 Rw = root.getRightWeight();
+                //Preparing print array
+                arr[size] = "Node: "+root.getLabel();
+                ++size;
             } else if (compareTo(Lw, Rw) < 0){
-                System.out.println("Go Right Marty -->");
-                System.out.println("Lw is = "+Rw);
+                //System.out.println("Go Right Marty -->");
+                //System.out.println("Lw is = "+Rw);
                 fun += Rw;
                 newRoot = root.getRightChild();
-                System.out.println("New root is: "+newRoot.getLabel());
+                //System.out.println("New root is: "+newRoot.getLabel());
                 root = newRoot;
                 Lchild = root.getLeftChild();
-                System.out.println("Left child of newRoot is: "+Lchild);
+                //System.out.println("Left child of newRoot is: "+Lchild);
                 Rchild = root.getRightChild();
-                System.out.println("Right child of newRoot is: "+Rchild);
+                //System.out.println("Right child of newRoot is: "+Rchild);
                 Lw = root.getLeftWeight();
                 Rw = root.getRightWeight();
-            } else if (compareTo(Lw, Rw) == 0)
-                System.out.println("Marty goes home because he can't decide which way to go.");
+            } else if (compareTo(Lw, Rw) == 0){
+                //System.out.println("Marty goes home because he can't decide which way to go.");
+            }
         }
         System.out.println("Largest Funness: "+fun);
-        print(newRoot);
-        
+        System.out.println(old_root);
+        for (int i = 0; i < size; ++i)
+            System.out.println(arr[i]);
     }
 }
